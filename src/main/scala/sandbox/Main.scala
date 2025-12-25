f053541e01c80b8bb5f6a2466d1723dc21a2ddc1
Jason Chukwu <jochuk95@gmail.com>
2025-12-25 11:57:33 -0800

    update

--- src/main/scala/sandbox/Main.scala
+++ src/main/scala/sandbox/Main.scala
@@ -3,8 +3,19 @@ package sandbox
 // import cats.syntax.all.*
 import printable.PrintableInstances._
 import printable.Printable
+import printable.PrintableSyntax._
+import cat.Cat
 
 @main
 def main(): Unit =
-  Printable.print(7)
-  Printable.print("Seven")
+  val stringSeven = "Seven"
+  val intSeven = 7
+  val cat = Cat("Pepper", 1, "Brown")
+
+  Printable.print(intSeven)
+  Printable.print(stringSeven)
+
+  // singleton object print method through Printable object
+  Printable.print(cat)
+  // print extension method implicit class def w/ a generic constructor
+  cat.print
--- src/main/scala/sandbox/Printable.scala
+++ src/main/scala/sandbox/Printable.scala
@@ -1,5 +1,7 @@
 package printable
 
+import cat.Cat
+
 trait Printable[A] {
   def format(value: A): String
 }
@@ -14,6 +16,16 @@ object PrintableInstances {
     new Printable[Int] {
       def format(value: Int): String = value.toString
     }
+
+  implicit val catPrinter: Printable[Cat] =
+      new Printable[Cat] {
+        def format(value: Cat): String =
+          val name = Printable.format(value.name)
+          val age = Printable.format(value.age)
+          val color = Printable.format(value.color)
+
+          s"${name} is a ${age} year-old ${color} cat"
+      }
 }
 
 object Printable {
@@ -23,5 +35,16 @@ object Printable {
 
   def print[A] (value: A)(implicit p: Printable[A]): Unit = {
      println(format(value))
+  }
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+
+  def format(implicit printer: Printable[A]): String =
+    printer.format(value)
+
+  def print(implicit printer: Printable[A]): Unit =
+    println(printer.format(value))
   }
 }
